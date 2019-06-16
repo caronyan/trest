d998efc144be529d7059e4fc8617d4630eb01cc4
caron <carony@163.com>
2019-06-16 22:13:45 +0800

    feat(util): add string utils

--- trest-common/src/main/java/org/carony/trest/common/util/StringUtils.java
+++ trest-common/src/main/java/org/carony/trest/common/util/StringUtils.java
@@ -1,8 +1,26 @@
 package org.carony.trest.common.util;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  * @author Caronyan
  * @date 2019-06-12
  */
 public class StringUtils {
+
+    /**
+     * generate url param string from param map
+     *
+     * @param paramMap key-value of params
+     * @return param string
+     */
+    public static String getUrlParamStrFromMap(Map<String, String> paramMap) {
+        return paramMap.entrySet().isEmpty()
+                ? ""
+                : ("?" + String.join("&", paramMap.entrySet().stream()
+                    .map(paramMapEntry -> String.format("%s=%s", paramMapEntry.getKey(), paramMapEntry.getValue()))
+                    .collect(Collectors.toList())));
+    }
+
 }
