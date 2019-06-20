3c834105197cfa3b788a0439d4c6811849df7055
caron <carony@163.com>
2019-06-20 17:13:22 +0800

    feat(algo): init sort function

--- trest-algo/src/main/java/org/carony/trest/algo/sort/InsertionSort.java
+++ trest-algo/src/main/java/org/carony/trest/algo/sort/InsertionSort.java
@@ -1,8 +1,17 @@
 package org.carony.trest.algo.sort;
 
 /**
+ * insertion sort
+ *
  * @author Caronyan
  * @date 2019-06-18
  */
 public class InsertionSort {
+
+    /**
+     * implementation of insertion sort
+     */
+    public static ISort insertionSort = (originalList, sortOrder) -> {
+        return null;
+    };
 }
--- trest-algo/src/main/java/org/carony/trest/algo/sort/QuickSort.java
+++ trest-algo/src/main/java/org/carony/trest/algo/sort/QuickSort.java
@@ -1,8 +1,17 @@
 package org.carony.trest.algo.sort;
 
 /**
+ * quick sort
+ *
  * @author Caronyan
  * @date 2019-06-18
  */
 public class QuickSort {
+
+    /**
+     * implementation of quick sort
+     */
+    public static ISort quickSort = (originalList, sortOrder) -> {
+        return null;
+    };
 }
