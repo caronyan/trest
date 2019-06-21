5aebf03a0b88c2e172e86a511d8feb5075ccf75f
caron <carony@163.com>
2019-06-21 16:02:31 +0800

    feat(algo): init sort function

--- trest-algo/src/main/java/org/carony/trest/algo/sort/SelectionSort.java
+++ trest-algo/src/main/java/org/carony/trest/algo/sort/SelectionSort.java
@@ -1,8 +1,17 @@
 package org.carony.trest.algo.sort;
 
 /**
+ * selection sort
+ *
  * @author Caronyan
  * @date 2019-06-18
  */
 public class SelectionSort {
+
+    /**
+     * implementation of selection sort
+     */
+    public static ISort selectionSort = (originalList, sortOrder) -> {
+        return null;
+    };
 }
--- trest-algo/src/main/java/org/carony/trest/algo/sort/ShellSort.java
+++ trest-algo/src/main/java/org/carony/trest/algo/sort/ShellSort.java
@@ -1,8 +1,17 @@
 package org.carony.trest.algo.sort;
 
 /**
+ * shell sort
+ *
  * @author Caronyan
  * @date 2019-06-18
  */
 public class ShellSort {
+
+    /**
+     * implementation of shell sort
+     */
+    public static ISort shellSort = (originalList, sortOrder) -> {
+        return null;
+    };
 }
